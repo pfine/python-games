98e35c09a60b551b855265750cdf38a986c10664
Piotr Grzywna <Piotr.Grzywna@pl.ibm.com>
2020-11-12 00:31:45 +0100

    add paddles and ball

--- pong2/pong.py
+++ pong2/pong.py
@@ -4,7 +4,33 @@ win = turtle.Screen()
 win.title("Pong by tutorial")
 win.bgcolor("black")
 win.setup(width=800, height=600)
-win.tracer(0)  #
+win.tracer(0)
+
+# Paddle A
+paddle_a = turtle.Turtle()
+paddle_a.speed(0) # set max speed
+paddle_a.shape("square") # by default 20px per 20px size
+paddle_a.shapesize(stretch_wid=5, stretch_len=1) # change to 20x100px
+paddle_a.color("white")
+paddle_a.penup()
+paddle_a.goto(-350,0)
+
+#  Paddle B
+paddle_b = turtle.Turtle()
+paddle_b.speed(0) # set max speed
+paddle_b.shape("square") # by default 20px per 20px size
+paddle_b.shapesize(stretch_wid=5, stretch_len=1) # change to 20x100px
+paddle_b.color("white")
+paddle_b.penup()
+paddle_b.goto(350,0)
+
+# Ball
+ball = turtle.Turtle()
+ball.speed(0) # set max speed
+ball.shape("square")
+ball.color("white")
+ball.penup()
+ball.goto(0,0)
 
 # Main game log
 while True:
