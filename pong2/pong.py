692dec688ccf441f1ed89494947f5a58f85a6e5f
Piotr Grzywna <Piotr.Grzywna@pl.ibm.com>
2020-11-11 12:15:26 +0100

    window is ready for work

---
+++ pong2/pong.py
@@ -0,0 +1,11 @@
+import turtle
+
+win = turtle.Screen()
+win.title("Pong by tutorial")
+win.bgcolor("black")
+win.setup(width=800, height=600)
+win.tracer(0) #
+
+# Main game log
+while True:
+    win.update()
