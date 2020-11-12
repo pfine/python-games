08946719ec95b9c02d50773f35e4cfb539259c6d
Piotr Grzywna <Piotr.Grzywna@pl.ibm.com>
2020-11-12 01:20:14 +0100

    testing using autopep8 on save

--- pong2/pong.py
+++ pong2/pong.py
@@ -8,29 +8,29 @@ win.tracer(0)
 
 # Paddle A
 paddle_a = turtle.Turtle()
-paddle_a.speed(0) # set max speed
-paddle_a.shape("square") # by default 20px per 20px size
-paddle_a.shapesize(stretch_wid=5, stretch_len=1) # change to 20x100px
+paddle_a.speed(0)  # set max speed
+paddle_a.shape("square")  # by default 20px per 20px size
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)  # change to 20x100px
 paddle_a.color("white")
 paddle_a.penup()
-paddle_a.goto(-350,0)
+paddle_a.goto(-350, 0)
 
 #  Paddle B
 paddle_b = turtle.Turtle()
-paddle_b.speed(0) # set max speed
-paddle_b.shape("square") # by default 20px per 20px size
-paddle_b.shapesize(stretch_wid=5, stretch_len=1) # change to 20x100px
+paddle_b.speed(0)  # set max speed
+paddle_b.shape("square")  # by default 20px per 20px size
+paddle_b.shapesize(stretch_wid=5, stretch_len=1)  # change to 20x100px
 paddle_b.color("white")
 paddle_b.penup()
-paddle_b.goto(350,0)
+paddle_b.goto(350, 0)
 
 # Ball
 ball = turtle.Turtle()
-ball.speed(0) # set max speed
+ball.speed(0)  # set max speed
 ball.shape("square")
 ball.color("white")
 ball.penup()
-ball.goto(0,0)
+ball.goto(0, 0)
 
 # Main game log
 while True:
